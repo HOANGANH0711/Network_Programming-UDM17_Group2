76d005b4e09b89baa05b10a0efa8d40ae5b74935
Lê Hoàng Anh <anhlh6037@ut.edu.vn>
2026-05-24 20:03:51 +0700

    feat: add RoomDTO.cs - data phong choi

---
+++ Code/Shared/DTO/RoomDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTO
+{
+    public class RoomDTO
+    {
+        public string RoomID { get; set; }
+        public string RoomName { get; set; }
+        public string OwerID { get; set; }
+        public string Player1ID { get; set; }
+        public string Player2ID { get; set; }
+        public bool IsPlaying { get; set; }
+        public bool IsFull { get; set; }
+        public RoomDTO()
+        {
+            IsPlaying = false;
+            IsFull = false;
+        }
+
+
+    }
+}
