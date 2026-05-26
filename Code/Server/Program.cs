6437a78a3f90c8be1743b85df0d6d1e24e48357c
Anhnguyet1204 <nguyettta8540.ut.edu.vn>
2026-05-26 19:26:45 +0700

    edit file Program.cs

--- Code/Server/Program.cs
+++ Code/Server/Program.cs
@@ -1,36 +1,10 @@
-﻿using Server.GameLogic;
+﻿namespace Server;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var game = new GameService();
-        game.StartGame();
-        Console.WriteLine("Game bắt đầu! X đi trước.\n");
-
-        // Giả lập vài nước đi
-        var moves = new[]
-        {
-            new Move(7, 7, CellState.X),
-            new Move(7, 8, CellState.O),
-            new Move(8, 7, CellState.X),
-            new Move(8, 8, CellState.O),
-            new Move(9, 7, CellState.X),
-            new Move(9, 8, CellState.O),
-            new Move(10, 7, CellState.X),
-            new Move(10, 8, CellState.O),
-            new Move(11, 7, CellState.X), // X thắng!
-        };
-
-        foreach (var move in moves)
-        {
-            string result = game.MakeMove(move);
-            Console.WriteLine($"[{move.Player}] ({move.Row},{move.Col}) → {result}");
-            if (result.StartsWith("WIN"))
-            {
-                Console.WriteLine($"\n🏆 {game.Winner} thắng!");
-                break;
-            }
-        }
+        Console.WriteLine("Server starting...");
+        Console.ReadLine();
     }
 }
