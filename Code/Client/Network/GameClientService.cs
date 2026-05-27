6ba05ce8ed1d8c47f070ef0260f3d165ac2b3093
Lê Hoàng Anh <anhlh6037@ut.edu.vn>
2026-05-27 21:36:11 +0700

    feat: add file GameClientService.cs and update file CommandType.cs

---
+++ Code/Client/Network/GameClientService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Enums;
+using Shared.Models;
+using Shared.DTO;
+using System.Text.Json;
+
+namespace Client.Network
+{
+    public class GameClientService
+    {
+        // EVENTS - Cac form/UI se lang nghe cac event nay
+        // Khi GameClientService nhan duoc Packet bang event thi UI se tu cap nhat
+
+        public event Action<List<UserDTO>> OnPlayerListReceived; //nhan danh sach nguoi choi
+        public event Action<List<RoomDTO>> OnRoomListReceived; //nhan danh sach phong
+        public event Action<GameDTO> OnGameStarted; //Game bat dau
+        public event Action<MoveDTO> OnMoveMade; // co nuoc di moi
+        public event Action<string> OnGameEnded; //Game ket thuc, thong bao nguoi thang
+        public event Action<string> OnError; //Thong bao loi
+        public event Action<UserDTO> OnInviteReveived; // nhan loi moi
+
+        //Goi ham moi khi nhan duoc packet tu server
+
+        public void HandlePacket(Packet packet)
+        {
+            switch (packet.Command)
+            {
+                // nhan danh sach nguoi choi trong lobby
+                case CommandType.PLAYER_LIST:
+                    var players = JsonSerializer.Deserialize<List<UserDTO>>(packet.Data);
+                    OnPlayerListReceived?.Invoke(players);
+                    break;
+                //nhanh danh sach phong
+                case CommandType.ROOM_LIST:
+                    var rooms = JsonSerializer.Deserialize<List<RoomDTO>>(packet.Data);
+                    OnRoomListReceived?.Invoke(rooms);
+                    break;
+                // nhan loi moi
+                case CommandType.INVITE:
+                    var inviter = JsonSerializer.Deserialize<UserDTO>(packet.Data);
+                    OnInviteReveived?.Invoke(inviter);
+                    break;
+                // Gamr bat dau
+                case CommandType.GAME_START:
+                    var game = JsonSerializer.Deserialize<GameDTO>(packet.Data);
+                    OnGameStarted?.Invoke(game);
+                    break;
+                // co nuoc do moi 
+                case CommandType.GAME_MOVE:
+                    var move = JsonSerializer.Deserialize<MoveDTO>(packet.Data);
+                    OnMoveMade?.Invoke(move);
+                    break;
+
+                case CommandType.GAME_END:
+                    OnGameEnded?.Invoke(packet.Data); // packet.Data chua ten nguoi thang
+                    break;
+                case CommandType.ERROR:
+                    OnError?.Invoke(packet.Data); // packet.Data chua thong bao loi
+                    break;
+            }
+        }
+
+    }
+}
