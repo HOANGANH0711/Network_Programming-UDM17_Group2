5b0116718f698aa3fc9a856515195a39f220c4b0
LAMGIAAN0805 <lamgiaan2001@gmail.com>
2026-05-31 00:28:41 +0700

    add file InviteDTO.cs

---
+++ Code/Shared/DTO/InviteDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Shared.DTO
+{
+    public class InviteDto
+    {
+        public string FromPlayerId { get; set; } = "";
+        public string FromPlayerName { get; set; } = "";
+        public string ToPlayerId { get; set; } = "";
+    }
+
+    public class GameStartDto
+    {
+        public string RoomId { get; set; } = "";
+        public string Player1Id { get; set; } = "";
+        public string Player1Name { get; set; } = "";
+        public string Player2Id { get; set; } = "";
+        public string Player2Name { get; set; } = "";
+    }
+}
