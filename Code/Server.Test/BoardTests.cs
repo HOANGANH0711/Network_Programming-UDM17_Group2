148f345b3ab7ebdec35ed9fa4f8197ae53317d88
Anhnguyet1204 <nguyettta8540.ut.edu.vn>
2026-06-02 16:47:21 +0700

    edit file Board.cs

---
+++ Code/Server.Test/BoardTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.GameLogic;
+
+namespace Server.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void PlaceMove_OnOccupiedCell_ShouldReturnFalse()
+        {
+            Board board = new Board();
+
+            board.PlaceMove(1, 1, CellState.X);
+
+            bool result =
+                board.PlaceMove(1, 1, CellState.O);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void InvalidPosition_ShouldReturnFalse()
+        {
+            Board board = new Board();
+
+            bool result =
+                board.PlaceMove(-1, 20, CellState.X);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Code/Server.Test/WinCheckerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server.GameLogic;
+
+namespace Server.Tests
+{
+    [TestClass]
+    public class WinCheckerTests
+    {
+        [TestMethod]
+        public void HorizontalWin_ShouldReturnTrue()
+        {
+            Board board = new Board();
+
+            for (int i = 0; i < 5; i++)
+            {
+                board.PlaceMove(0, i, CellState.X);
+            }
+
+            bool result =
+                WinChecker.CheckWin(board, 0, 4, CellState.X);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
